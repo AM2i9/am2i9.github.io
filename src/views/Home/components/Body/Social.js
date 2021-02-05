edf7f8a0f894298903a9a18a2e53459b32c256d8
AM2i9 <brennanpatrick20@gmail.com>
2021-02-05 15:33:27 -0500

    Social buttons now open in new tab

--- src/views/Home/components/Body/Social.js
+++ src/views/Home/components/Body/Social.js
@@ -7,10 +7,10 @@ function Social() {
         <div className="Social">
             <ul>
                 <li>
-                    <a href="https://github.com/AM2i9"><img id="github-link" src="GitHub-Mark-Light-64px.png" alt="Github"></img></a>
+                    <a href="https://github.com/AM2i9" target="_blank" rel="noopener noreferrer"><img id="github-link" src="GitHub-Mark-Light-64px.png" alt="Github"></img></a>
                 </li>
                 <li>
-                    <a href="https://www.youtube.com/channel/UCfLCmE1BPuZm_xX3hxRMNdQ"><img id="youtube-link" src="yt_icon_mono_dark.png" alt="Youtube"></img></a>
+                    <a href="https://www.youtube.com/channel/UCfLCmE1BPuZm_xX3hxRMNdQ" target="_blank" rel="noopener noreferrer"><img id="youtube-link" src="yt_icon_mono_dark.png" alt="Youtube"></img></a>
                 </li>
             </ul>
         </div>
