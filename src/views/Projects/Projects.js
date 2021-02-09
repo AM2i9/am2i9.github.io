41cd5c0a21fcc895b71c0c8a6bf4820835188600
AM2i9 <brennanpatrick20@gmail.com>
2021-02-08 20:27:34 -0500

    updated things with projects tab

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -9,7 +9,7 @@ class Projects extends React.Component {
             <div className = "Projects">
                 <center>
                     <React.StrictMode>
-                        <Project name="Project 1" />
+                        <Project name="Project 1" description="This is a project in order to test the projects page" github_link="https://github.com/AM2i9/am2i9.github.io" repl_link="https://repl.it/~"/>
                         <Project name="Project 2" />
                     </React.StrictMode>
                 </center>
