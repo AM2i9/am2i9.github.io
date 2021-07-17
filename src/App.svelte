26cfa71fe3074317adf34a18014c0965a2eb2354
Patrick Brennan <brennanpatrick20@gmail.com>
2021-07-17 18:01:56 -0400

    Starting to make a new site, looks a lot better

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import OldSite from './components/OldSite';
+
+	import Logo from './components/Logo';
+</script>
+
+<main>
+	<OldSite/>
+	<Logo />
+</main>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,89 @@
+<style>
+
+	.logo {
+		text-align: center;
+		margin-bottom: 0px;
+		margin-top: 0px;
+	}
+
+	.logo #username {
+		animation-duration: 1s;
+		animation-name: fade-slide-top;
+		font-size: 5rem;
+	}
+
+	.logo #full-name {
+		animation-duration: 1s;
+		animation-name: fade-slide-bottom;
+		font-size: 2rem;
+	}
+
+	.logo #seperator {
+		animation-duration: 2s;
+		animation-name: x-fade-scale-in;
+
+		width: 350px;
+		height: 3px;
+		background-color: #fff;
+	}
+
+	.logo img {
+
+		animation-duration: 3s;
+		animation-name: img-fade-in;
+
+		margin-top: 20px;
+		width: 400px;
+		border-radius: 50%;
+	}
+
+	@keyframes x-fade-scale-in {
+		0%, 20% {
+			transform: scaleX(0);
+			opacity: 0%;
+		}
+		to {
+			transform: scaleX(1);
+			opacity: 100%;
+		}
+	}
+
+	@keyframes fade-slide-top {
+		0%, 20% {
+			transform: translateY(-50px);
+			opacity: 0%;
+		}
+		to {
+			transform: translateY(0px);
+			opacity: 100%;
+		}
+	}
+
+	@keyframes fade-slide-bottom {
+		0%, 20% {
+			transform: translateY(50px);
+			opacity: 0%;
+		}
+		to {
+			transform: translateY(0px);
+			opacity: 100%;
+		}
+	}
+
+	@keyframes img-fade-in {
+		0%, 40% {
+			opacity: 0%;
+		}
+		to {
+			opacity: 100%;
+		}
+	}
+
+</style>
+
+<div class="logo">
+	<h1 id="username" class="logo">AM2i9</h1>
+	<hr id="seperator" class="logo"/>
+	<h2 id="full-name" class="logo">Patrick Brennan</h2>
+	<img src="https://github.com/AM2i9.png" alt="Current Github Profile Picture">
+</div>
